1c3569c846b1a3933b1c2a79b9fde1d3b0865fa6
Rob Stryker <rob@oxbeef.net>
2021-03-24 14:14:22 -0400

    Export more things
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ import { IWizard } from "./IWizard";
 import { IWizardPage } from "./IWizardPage";
 import { IWizardWorkflowManager } from "./IWizardWorkflowManager";
 import { WebviewWizard } from "./WebviewWizard";
+import { WizardPageValidator, WizardPageFieldOptionProvider, WizardDefinition, 
+    WizardPageDefinition, WizardPageFieldDefinition } from "./WebviewWizard";
 
-export { IWizard, IWizardPage, IWizardWorkflowManager, WebviewWizard};
+export { IWizard, IWizardPage, IWizardWorkflowManager, WebviewWizard,
+    WizardPageValidator, WizardPageFieldOptionProvider, 
+    WizardDefinition, WizardPageDefinition, WizardPageFieldDefinition};
