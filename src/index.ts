4e2ca16847ca376eed683cad21bf4a70132dc62b
Rob Stryker <rob@oxbeef.net>
2021-06-14 14:59:28 -0400

    Test in production.
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { IWizard } from "./IWizard";
 import { IWizardPage } from "./IWizardPage";
 import { IWizardWorkflowManager, PerformFinishResponse } from "./IWizardWorkflowManager";
 import { Template } from "./pageImpl";
-import { BUTTONS, SEVERITY, ValidatorResponseItem, WebviewWizard } from "./WebviewWizard";
+import { BUTTONS, SEVERITY, ValidatorResponseItem, WebviewWizard, UPDATE_TITLE } from "./WebviewWizard";
 import { WizardPageValidator, WizardPageFieldOptionProvider, WizardDefinition, 
     WizardPageDefinition, WizardPageFieldDefinition, WizardPageSectionDefinition, ValidatorResponse } from "./WebviewWizard";
 
@@ -10,4 +10,4 @@ export { IWizard, IWizardPage, IWizardWorkflowManager, WebviewWizard,
     WizardPageValidator, WizardPageFieldOptionProvider, 
     WizardDefinition, WizardPageDefinition, WizardPageFieldDefinition, 
     WizardPageSectionDefinition, ValidatorResponse, Template,
-    BUTTONS, SEVERITY, ValidatorResponseItem, PerformFinishResponse};
+    UPDATE_TITLE, BUTTONS, SEVERITY, ValidatorResponseItem, PerformFinishResponse};
